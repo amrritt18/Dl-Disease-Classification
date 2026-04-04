b27f98bf247fdf6acd751f7f9b8bb89cfa682b2a
amrritt18 <amrritt1804@gmail.com>
2026-04-04 17:17:06 +0530

    Model Training done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kidney_disease_classifier import logger
 from kidney_disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidney_disease_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-# from kidney_disease_classifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from kidney_disease_classifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 # from kidney_disease_classifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
@@ -31,16 +31,16 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Training"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_trainer = ModelTrainingPipeline()
-#    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
